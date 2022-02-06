c961b9569eab16ceca8e5d29998ada2b68a14767
AlehTrafimau <trofimovlega@gmail.com>
2022-02-07 01:01:46 +0300

    Improve find firstname performance with dictionary.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -6,6 +6,8 @@ namespace FileCabinetApp
     {
         private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>();
 
+        private readonly Dictionary<string, List<FileCabinetRecord>> firstNameDictionary = new Dictionary<string, List<FileCabinetRecord>>();
+
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, char serieOfPassNumber, short passNumber, decimal currentBankAccount)
         {
             var record = new FileCabinetRecord
@@ -20,6 +22,14 @@ namespace FileCabinetApp
             };
 
             this.list.Add(record);
+            if (this.firstNameDictionary.ContainsKey(firstName.ToUpperInvariant()))
+            {
+                this.firstNameDictionary[firstName.ToUpperInvariant()].Add(record);
+            }
+            else
+            {
+                this.firstNameDictionary.Add(firstName.ToUpperInvariant(), new List<FileCabinetRecord>() { record });
+            }
 
             return record.Id;
         }
@@ -37,25 +47,56 @@ namespace FileCabinetApp
 
         public void EditRecord(int id, string firstName, string lastName, DateTime dateOfBirth, char serieOfPassNumber, short passNumber, decimal currentBankAccount)
         {
-                int numberOfRecordForEdit = id - 1;
-                this.list[numberOfRecordForEdit].DateOfBirth = dateOfBirth;
-                this.list[numberOfRecordForEdit].FirstName = firstName;
-                this.list[numberOfRecordForEdit].LastName = lastName;
-                this.list[numberOfRecordForEdit].PassNumber = passNumber;
-                this.list[numberOfRecordForEdit].SerieOfPassNumber = serieOfPassNumber;
-                this.list[numberOfRecordForEdit].CurrentBankAccount = currentBankAccount;
-                Console.WriteLine($"Record #{id} is updated");
+            int numberOfRecordForEdit = id - 1;
+
+            string? firstNameBeforeEdit = this.list[numberOfRecordForEdit].FirstName;
+            this.list[numberOfRecordForEdit].FirstName = firstName;
+
+            if (firstNameBeforeEdit != null && firstNameBeforeEdit.ToUpperInvariant() != firstName.ToUpperInvariant())
+            {
+                FileCabinetRecord recordForMove = new FileCabinetRecord();
+
+                for (int i = 0; i < this.firstNameDictionary[firstNameBeforeEdit.ToUpperInvariant()].Count; i++)
+                {
+                    if (this.firstNameDictionary[firstNameBeforeEdit.ToUpperInvariant()][i].Id == id)
+                    {
+                        recordForMove = this.firstNameDictionary[firstNameBeforeEdit.ToUpperInvariant()][i];
+                        this.firstNameDictionary[firstNameBeforeEdit.ToUpperInvariant()].RemoveAt(i);
+                        if (this.firstNameDictionary[firstNameBeforeEdit.ToUpperInvariant()].Count == 0)
+                        {
+                            this.firstNameDictionary.Remove(firstNameBeforeEdit.ToUpperInvariant());
+                        }
+
+                        break;
+                    }
+                }
+
+                if (this.firstNameDictionary.ContainsKey(firstName.ToUpperInvariant()))
+                {
+                    this.firstNameDictionary[firstName.ToUpperInvariant()].Add(recordForMove);
+                }
+                else
+                {
+                    this.firstNameDictionary.Add(firstName.ToUpperInvariant(), new List<FileCabinetRecord>() { recordForMove });
+                }
+            }
+
+            this.list[numberOfRecordForEdit].DateOfBirth = dateOfBirth;
+            this.list[numberOfRecordForEdit].LastName = lastName;
+            this.list[numberOfRecordForEdit].PassNumber = passNumber;
+            this.list[numberOfRecordForEdit].SerieOfPassNumber = serieOfPassNumber;
+            this.list[numberOfRecordForEdit].CurrentBankAccount = currentBankAccount;
+
+            Console.WriteLine($"Record #{id} is updated");
         }
 
         public FileCabinetRecord[] FindByFirstName(string firstName)
         {
             List<FileCabinetRecord> result = new List<FileCabinetRecord>();
-            foreach (FileCabinetRecord currentRecord in this.list)
+
+            if (this.firstNameDictionary.ContainsKey(firstName.ToUpperInvariant()))
             {
-                if (currentRecord.FirstName != null && currentRecord.FirstName.ToUpperInvariant() == firstName.ToUpperInvariant())
-                {
-                    result.Add(currentRecord);
-                }
+                result = this.firstNameDictionary[firstName.ToUpperInvariant()];
             }
 
             return result.ToArray();
