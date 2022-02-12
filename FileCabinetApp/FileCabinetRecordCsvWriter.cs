47430ac0dbc802eb05178ea79c33af3a48c36776
AlehTrafimau <trofimovlega@gmail.com>
2022-02-12 16:08:06 +0300

    Add export of file cabinet records to CSV file.

---
+++ FileCabinetApp/FileCabinetRecordCsvWriter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace FileCabinetApp
+{
+    /// <summary>
+    /// Convert records to scv format.
+    /// </summary>
+    internal class FileCabinetRecordCsvWriter
+    {
+        private readonly TextWriter writer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileCabinetRecordCsvWriter"/> class.
+        /// </summary>
+        /// <param name="writer">The writer.</param>
+        internal FileCabinetRecordCsvWriter(TextWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        /// <summary>
+        /// Writes the specified records.
+        /// </summary>
+        /// <param name="records">The records.</param>
+        public void Write(FileCabinetRecord[] records)
+        {
+            StringBuilder csvFormat = new ();
+
+            foreach (FileCabinetRecord i in records)
+            {
+                csvFormat.AppendLine(CultureInfo.InvariantCulture, $"{i.FirstName}, {i.LastName}, {i.DateOfBirth}, {i.SerieOfPassNumber}, {i.PassNumber}, {i.BankAccount}");
+            }
+
+            using (this.writer)
+            {
+                this.writer.Write(csvFormat);
+            }
+        }
+    }
+}
---
+++ FileCabinetApp/FileCabinetServiceSnapshot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCabinetApp
+{
+    /// <summary>
+    /// Service to storage states of records.
+    /// </summary>
+    public class FileCabinetServiceSnapshot
+    {
+        /// <summary>
+        /// Storages snapshots of records status.
+        /// </summary>
+        private readonly FileCabinetRecord[] latestSnapshotOfRecordsState;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileCabinetServiceSnapshot"/> class.
+        /// </summary>
+        /// <param name="newSnapshot">The new snapshot.</param>
+        internal FileCabinetServiceSnapshot(FileCabinetRecord[] newSnapshot)
+        {
+            this.latestSnapshotOfRecordsState = newSnapshot;
+        }
+
+        /// <summary>
+        /// Saves to CSV.
+        /// </summary>
+        /// <param name="streamWriter">The stream writer.</param>
+        internal void SaveToCsv(StreamWriter streamWriter)
+        {
+            FileCabinetRecordCsvWriter saveSCV = new (streamWriter);
+            saveSCV.Write(this.latestSnapshotOfRecordsState);
+        }
+    }
+}
