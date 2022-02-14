b28f3f6d32a67d9272bf6ac118a6a41ec3c63652
AlehTrafimau <trofimovlega@gmail.com>
2022-02-15 01:27:48 +0300

    Add the following command line options: "--output-type="(-t), "--output="(-o), "--records-amount="(-a), "--start-id="(-i).

--- FileCabinetGenerator/FileCabinetGenerator.cs
+++ FileCabinetGenerator/FileCabinetGenerator.cs
@@ -1,6 +1,111 @@
-﻿namespace FileCabinetApp
+﻿using System.Text.RegularExpressions;
+
+namespace FileCabinetApp
 {
+    /// <summary>
+    /// Program for autogenerating new records and performing operations with them to export csv, xml format, write to fily system..
+    /// </summary>
     public static class FileCabinetGenerator
     {
+        private static int startId;
+        private static int numberOfRecords;
+        private static string pathToFile = string.Empty;
+
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            SetConsoleParameters(args);
+        }
+
+        private static void SetConsoleParameters(string[] args)
+        {
+            if (args == null)
+            {
+                Console.WriteLine("Enter parameters for record generate");
+                return;
+            }
+
+            if (args.Length == 4)
+            {
+                if (Regex.IsMatch(args[0].ToLowerInvariant(), @"^--output-type=[c|x]{1}[s|m]{1}[v|l]{1}$"))
+                {
+                    string format = args[0][^3..];
+                }
+
+                if (Regex.IsMatch(args[1].ToLowerInvariant(), @"^--output=\S*$"))
+                {
+                    pathToFile = args[1][9..];
+                }
+
+                if (Regex.IsMatch(args[2].ToLowerInvariant(), @"^--records-amount=\d+$"))
+                {
+                    bool isNumber = int.TryParse(args[2][17..], out int number);
+                    if (isNumber)
+                    {
+                        numberOfRecords = number;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid parameter for \"--records-amount\"  {args[2][18..]}");
+                    }
+                }
+
+                if (Regex.IsMatch(args[3].ToLowerInvariant(), @"^--start-id=\d+$"))
+                {
+                    bool isNumber = int.TryParse(args[3][11..], out int number);
+                    if (isNumber)
+                    {
+                        startId = number;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid parameter for \"--start-id\"  {args[3][12..]}");
+                    }
+                }
+            }
+            else if (args.Length == 8)
+            {
+                if (args[0].ToLowerInvariant() == "-t")
+                {
+                    string format = args[1];
+                }
+
+                if (args[2].ToLowerInvariant() == "-o")
+                {
+                    pathToFile = args[3];
+                }
+
+                if (args[4].ToLowerInvariant() == "-a")
+                {
+                    bool isNumber = int.TryParse(args[5], out int number);
+                    if (isNumber)
+                    {
+                        numberOfRecords = number;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid parameter for \"-a\"  {args[5]}");
+                    }
+                }
+
+                if (args[6].ToLowerInvariant() == "-i")
+                {
+                    bool isNumber = int.TryParse(args[7], out int number);
+                    if (isNumber)
+                    {
+                        startId = number;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid parameter for \"--start-id\"  {args[7]}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid parameters");
+            }
+        }
     }
 }
