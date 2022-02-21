8d09f1b2634afc4bf129ddf417dcb1693a67085b
AlehTrafimau <trofimovlega@gmail.com>
2022-02-21 19:59:23 +0300

    Apply the Chain of Responsibility template. In this aim add new directory CommandHandlers and add here AppCommandRequest.cs, CommandHandler.cs, Commandhandler.cs, ICommandHandler.cs files.

---
+++ FileCabinetApp/CommandHandlers/CommandHandlerBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCabinetApp.CommandHandlers
+{
+    /// <summary>
+    /// Consinsts of a set of methods to work with handlers.
+    /// </summary>
+    public abstract class CommandHandlerBase : ICommandHandler
+    {
+        private ICommandHandler? nextHandler;
+
+        /// <summary>
+        /// Sets the next handler for current command.
+        /// </summary>
+        /// <param name="nextHandler">The next handler.</param>
+        public void SetNext(ICommandHandler nextHandler)
+        {
+            this.nextHandler = nextHandler;
+        }
+
+        /// <summary>
+        /// Defines a handle for handling current request.
+        /// </summary>
+        /// <param name="handlingRequest">The request to hadnling.</param>
+        public abstract void Handle(AppCommandRequest handlingRequest);
+    }
+}
